7f10d86cef8cb4fa8f0d31f0eb4044afbea08e9c
Larisa <larisa.berger@gmail.com>
2012-03-05 21:34:43 -0500

    piped javascript var to html

--- ps1-translate_game/contents/translate_game.js
+++ ps1-translate_game/contents/translate_game.js
@@ -1,10 +1,11 @@
 // This allows the Javascript code inside this block to only run when the page
 // has finished loading in the browser.
 $(function() {
-	var lang_to		= "English";
-	var lang_from		= "Spanish";
-	var current_dict	= dicts[lang_to][lang_from]; // keys: words in @lang_to, values: corresponding words in @lang_from 	
+		var lang_to		= "English";
+		var lang_from		= "Spanish";
+		var current_dict	= dicts[lang_to][lang_from]; // keys: words in @lang_to, values: corresponding words in @lang_from 	
 
-	// Your code here
+		$('.toLang').text(lang_to);
+		$('.fromLang').text(lang_from);
 
     });
