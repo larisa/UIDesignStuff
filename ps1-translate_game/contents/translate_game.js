c020cbf49c7700d45b7b939485e2074ed256cc52
Larisa <larisa.berger@gmail.com>
2012-03-06 03:49:17 -0500

    implemented keeping track of correct an dincorrect answers. modified prepareNextWord to do all the work of checking the word also. Also appending successfully but to the existing rows.

--- ps1-translate_game/contents/translate_game.js
+++ ps1-translate_game/contents/translate_game.js
@@ -1,34 +1,60 @@
 // This allows the Javascript code inside this block to only run when the page
 // has finished loading in the browser.
+
+var allFromWords = new Array();
+var allGuesses = new Array();
+var allToWords = new Array();
+
+
 $(function() {
 		var lang_to		= "English";
 		var lang_from		= "Spanish";
 		var current_dict	= dicts[lang_to][lang_from]; // keys: words in @lang_to, values: corresponding words in @lang_from 	
 
-		
 		//supply the To and From Languages
 		$('.toLang').text(lang_to);
 		$('.fromLang').text(lang_from);
 		
-		//pick a random word.
+
 		var randomToWord = pickRandomProperty(current_dict);
+		allToWords.push(randomToWord);
 		var randomFromWord = current_dict[randomToWord]; 
+		allFromWords.push(randomFromWord);
+		
+		//display Prompt
 		$('.PromptWord').text(randomFromWord);
 
-		//get user's input and check it
+		//get user's input
 		$('#userGuess').submit(function(){
 				console.log("form has submitted!");
 				var guess = $("input[name=userInput]").val();
 				console.log("YOU GUESSED " + guess);
-				if (guess==randomToWord)
-						console.log("Correct!");
-				else
-						console.log("WRONG");
+				prepareNextWord(randomFromWord, guess, randomToWord);
 				return false;
 		});
 
     });
 
+
+//prompt for another word, clear the input
+function prepareNextWord(prompt, guess, answer){
+		if (guess==answer){
+				console.log("CORRECT!");
+				$('.correctLangFrom').append(prompt);
+				$('.correctLangTo').append(answer);
+				return true;
+		}
+		else{
+				console.log("WRONG!");
+				$('.wrongLangFrom').append(prompt);
+				$('.wrongLangTo').append(guess);
+				$('.actualTo').append(answer);
+				return false;
+		}
+		
+
+}
+				
 
 
 //get a random element
