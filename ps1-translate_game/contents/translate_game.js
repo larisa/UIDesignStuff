700b099ebfea0f96142e3d2fa869379d2caddafc
Larisa <larisa.berger@gmail.com>
2012-03-05 23:29:57 -0500

    succesfully checking inputted words.

--- ps1-translate_game/contents/translate_game.js
+++ ps1-translate_game/contents/translate_game.js
@@ -5,6 +5,7 @@ $(function() {
 		var lang_from		= "Spanish";
 		var current_dict	= dicts[lang_to][lang_from]; // keys: words in @lang_to, values: corresponding words in @lang_from 	
 
+		
 		//supply the To and From Languages
 		$('.toLang').text(lang_to);
 		$('.fromLang').text(lang_from);
@@ -14,11 +15,15 @@ $(function() {
 		var randomFromWord = current_dict[randomToWord]; 
 		$('.PromptWord').text(randomFromWord);
 
-		//get user's input
+		//get user's input and check it
 		$('#userGuess').submit(function(){
 				console.log("form has submitted!");
 				var guess = $("input[name=userInput]").val();
-				console.log(guess);
+				console.log("YOU GUESSED " + guess);
+				if (guess==randomToWord)
+						console.log("Correct!");
+				else
+						console.log("WRONG");
 				return false;
 		});
 
